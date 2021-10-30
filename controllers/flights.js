c57c1aba1b2692cc8c738bee94810f9ffdaef89d
breyshaw <dawsonbrey@gmail.com>
2021-10-30 18:40:52 -0400

    create create functionality

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,11 +1,25 @@
 //THIS IS MY FLIGHTS CONTROLLER
 
+import { Flight } from '../models/flight.js'
+
 //.new function (this is being called by the /new flights route)
 //It renders the flights/new VIEW
 function newFlight(req, res) {
     res.render('flights/new')
 }
+//The model is responsible for creating data. Need access to the model. Imported above.
+
+function create(req, res) {
+    const flight = new Flight(req.body)
+    flight.save(function (err) {
+        if (err) {
+            return res.redirect('/flights/new')
+        }
+        res.redirect('/flights/new')
+})
+}
 
 export {
-    newFlight as new
-}
+        newFlight as new,
+        create
+    }
--- routes/flights.js
+++ routes/flights.js
@@ -5,8 +5,15 @@ import { Router } from 'express'
 import * as flightsCtrl from '../controllers/flights.js'
 const router = Router()
 
+// router.get('/', flightsCtrl.index)
+
 //FLIGHTS ROUTES
+// localhost:300/flights/new
 router.get('/new', flightsCtrl.new)
+
+//localhost:3000/flights (this is the endpoint)
+//This is so the created flight goes to the movies route (movies is implied)
+router.post('/', flightsCtrl.create)
 
 export {
   router
